6a66db3fc4008ee2732b13628c51c808a78131bd
Maciej Satkiewicz <msatkiewicz@gmail.com>
2026-04-04 22:48:49 +0200

    add pullbacks to eval

--- saliency_evaluation/pointing_game_eval.py
+++ saliency_evaluation/pointing_game_eval.py
@@ -1,7 +1,15 @@
-from saliency_utils.pointing_game_utils import GridPointingGame
-from saliency_utils.Explainer import BcosExplainer, AttentionExplainer, GradientNPropabationExplainer, OcclusionExplainer, ShapleyValueExplainer, LimeExplainer
-from argparse import ArgumentParser
 import os
+from argparse import ArgumentParser
+
+from saliency_utils.Explainer import (
+    AttentionExplainer,
+    BcosExplainer,
+    GradientNPropabationExplainer,
+    LimeExplainer,
+    OcclusionExplainer,
+    ShapleyValueExplainer,
+)
+from saliency_utils.pointing_game_utils import GridPointingGame
 
 EXPLANATION_METHODS = {
     "Bcos": BcosExplainer,
@@ -16,7 +24,10 @@ EXPLANATION_METHODS = {
     "ShapleyValue": ShapleyValueExplainer,
     "KernelShap": ShapleyValueExplainer,
     "Lime": LimeExplainer,
+    "Pullback": GradientNPropabationExplainer,
+    "PullbackAscent": GradientNPropabationExplainer,
 }
+
 
 def main(args):
 
@@ -31,7 +42,7 @@ def main(args):
     args.seed = int(args.seed) if args.seed else None
     args.shap_n_samples = int(args.shap_n_samples) if args.shap_n_samples else None
     args.split_ratio = float(args.split_ratio) if args.split_ratio else None
-    
+
     pointing_game = GridPointingGame(
         model_name_or_path=args.model_dir,
         dataset=args.dataset_name,
@@ -51,10 +62,10 @@ def main(args):
     # Initialize the explainer
     all_methods = EXPLANATION_METHODS.keys()
     if args.methods:
-        attribution_methods = args.methods.replace(' ', '').split(',')     
+        attribution_methods = args.methods.replace(" ", "").split(",")
     else:
         attribution_methods = all_methods  # Use all methods if none specified
-    
+
     for method in attribution_methods:
         print(f"\nRunning {method} explainer...")
         explanation_path = os.path.join(args.output_dir, f"{method}_explanations.json")
@@ -70,26 +81,95 @@ def main(args):
         )
 
 
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Evaluate the faithfulness for rationales using pointing game methods."
+    )
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description='Evaluate the faithfulness for rationales using pointing game methods.')
-
-    parser.add_argument('--dataset_name', type=str, default='stanfordnlp/imdb', help='Name of the HuggingFace dataset to use') #fancyzhx/ag_news, stanfordnlp/imdb
-    parser.add_argument('--split', type=str, default='test', help='Dataset split to use (e.g., train, test)')
-    parser.add_argument('--split_ratio', type=float, default=0.5, help='Split ratio for test dataset')
-    parser.add_argument('--model_dir', type=str, default='models/bert_base_imdb_512', help='Name of the pre-trained model')
-    parser.add_argument('--load_pointing_game_examples_path', type=str, default=None, help='Path to load pointing game examples')
-    parser.add_argument('--save_pointing_game_examples_path', type=str, default=None, help='Path to save pointing game examples')
-    parser.add_argument('--num_labels', type=int, default=2, help='Number of labels in the classification')
-    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for DataLoader')
-    parser.add_argument('--max_length', type=int, default=256, help='Maximum sequence length for tokenization')
-    parser.add_argument('--num_examples', type=int, default=-1, help='Number of examples to process (-1 for all)')
-    parser.add_argument('--baseline', type=str, default='pad', help='Baseline for the attribution methods, select from zero, mask, pad')
-    parser.add_argument('--methods', type=str, default=None, help='List of attribution methods to use separated by commas')
-    parser.add_argument('--output_dir', type=str, default='baseline_results/all_methods_1000_examples_256_pointing_game_results', help='Directory to save the output files')
-    parser.add_argument('--seed', type=int, default=42, help='Random seed for reproducibility')
-    parser.add_argument('--shap_n_samples', type=int, default=25, help='Number of samples for Shapley Value Sampling')
-    parser.add_argument('--relative', action='store_true', help='explain relative logits')
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        default="stanfordnlp/imdb",
+        help="Name of the HuggingFace dataset to use",
+    )  # fancyzhx/ag_news, stanfordnlp/imdb
+    parser.add_argument(
+        "--split",
+        type=str,
+        default="test",
+        help="Dataset split to use (e.g., train, test)",
+    )
+    parser.add_argument(
+        "--split_ratio", type=float, default=0.5, help="Split ratio for test dataset"
+    )
+    parser.add_argument(
+        "--model_dir",
+        type=str,
+        default="models/bert_base_imdb_512",
+        help="Name of the pre-trained model",
+    )
+    parser.add_argument(
+        "--load_pointing_game_examples_path",
+        type=str,
+        default=None,
+        help="Path to load pointing game examples",
+    )
+    parser.add_argument(
+        "--save_pointing_game_examples_path",
+        type=str,
+        default=None,
+        help="Path to save pointing game examples",
+    )
+    parser.add_argument(
+        "--num_labels",
+        type=int,
+        default=2,
+        help="Number of labels in the classification",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=1, help="Batch size for DataLoader"
+    )
+    parser.add_argument(
+        "--max_length",
+        type=int,
+        default=256,
+        help="Maximum sequence length for tokenization",
+    )
+    parser.add_argument(
+        "--num_examples",
+        type=int,
+        default=-1,
+        help="Number of examples to process (-1 for all)",
+    )
+    parser.add_argument(
+        "--baseline",
+        type=str,
+        default="pad",
+        help="Baseline for the attribution methods, select from zero, mask, pad",
+    )
+    parser.add_argument(
+        "--methods",
+        type=str,
+        default=None,
+        help="List of attribution methods to use separated by commas",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="baseline_results/all_methods_1000_examples_256_pointing_game_results",
+        help="Directory to save the output files",
+    )
+    parser.add_argument(
+        "--seed", type=int, default=42, help="Random seed for reproducibility"
+    )
+    parser.add_argument(
+        "--shap_n_samples",
+        type=int,
+        default=25,
+        help="Number of samples for Shapley Value Sampling",
+    )
+    parser.add_argument(
+        "--relative", action="store_true", help="explain relative logits"
+    )
 
     args = parser.parse_args()
     main(args)
